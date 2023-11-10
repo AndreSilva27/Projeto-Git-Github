e5470e57ce00c7744c10283fe94f3b28117ad682
AndreSilva27 <ancris2725@hotmail.com>
2023-11-10 10:08:45 -0300

    lincando javascript no html

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Cris-Modas")
+
+const elements = document.getElementsByClassName("logo-loja")
+
+console.log(elements)
+
+const section = document.getElementsByTagName("section")
+
+console.log(section)
